a1c7006972805954646d54b35f55a0129738b38e
Fabien <cacrfabfr@gmail.com>
2021-11-02 10:59:35 +0100

    started to pin the first 3 parts

--- js/main.js
+++ js/main.js
@@ -1,16 +1,24 @@
 gsap.registerPlugin(ScrollTrigger);
 
-function init(){
 
+function init(){
+  
   // Move the part 3 to cover the part 2
   gsap.set('.part3', {
     y: () => {
-      return -(document.querySelector('.pen-top').clientHeight - 22);
+      return -(document.querySelector('.pen-top').clientHeight - 22)
+    },
+    scrollTrigger: {
+      id: 'pen-body',
+      trigger: '.part3',
+      start: 'top bottom-=270px',
+      end: `+=${(document.querySelector('.pen-top').clientHeight - 22)}`,
+      pin: true,
+      markers: true,
     }
   })
-
+  
 }
-
 window.addEventListener('load', function(){
     init();
 });
