d7085336964fc195a43c860cc6c0270e9e7a1537
Fabien <cacrfabfr@gmail.com>
2021-11-02 11:39:02 +0100

    finished the pen body and refactoring

--- js/main.js
+++ js/main.js
@@ -4,7 +4,6 @@ function getTopPartsHeight() {
   return document.querySelector('.pen-top').clientHeight - 22
 }
 
-
 function init(){
   
   // Move the part 3 to cover the part 2
@@ -22,19 +21,30 @@ function init(){
     }
   });
 
-  gsap.set('.part4', {y: '-547px'});
-  gsap.set('.part5', {y: '-722px'});
-  gsap.set('.part6', {y: '-842px'});
+  const partTopOffset = [547, 722, 842];
 
-  // Tween the tip of the pen back to 0
-  gsap.to('.part6', {y: 0, ease: 'none', scrollTrigger: {
-    trigger: '.pen-body',
-    start: 'top bottom-=640px',
-    end: '+=842px',
-    scrub: true,
-    markers: true,
-  }});
-  
+  function fixPart(el, offset, index) {
+
+    gsap.set(el, {y: -offset});
+
+    // Tween the tip of the pen back to 0
+    gsap.to(el, {y: 0, ease: 'none', scrollTrigger: {
+      trigger: '.pen-body',
+      start: 'top bottom-=640px',
+      end: `+=${offset}`,
+      scrub: true,
+      markers: true,
+    }});
+
+  }
+
+  gsap.utils.toArray(['.part4', '.part5', '.part6']).forEach((part, index) => {
+
+    fixPart(part, partTopOffset[index], index);
+
+  })
+
+
 }
 window.addEventListener('load', function(){
     init();
